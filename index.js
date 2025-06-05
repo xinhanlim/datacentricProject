421671a81bc67f7fda5a97531fb9f902a542dedc
xinhanlim <xhxhan00@gmail.com>
2025-06-05 19:16:16 +0800

    added title,genre under search

--- index.js
+++ index.js
@@ -21,53 +21,56 @@ async function main() {
   let db = await connect(mongoUri, dbname);
 
   //get all movies
-  app.get("/movies", async (req,res) => {
-    try{
-        const movies = await db.collection("movies").find().project({
-            title:1,
-            genre:1,
-            duration:1,
-            rating:1,
-            cast:1,
-            director:1,
-            synopsis:1,
-        }).toArray();
+  app.get("/movies", async (req, res) => {
+    try {
+      const movies = await db
+        .collection("movies")
+        .find()
+        .project({
+          title: 1,
+          genre: 1,
+          duration: 1,
+          rating: 1,
+          cast: 1,
+          director: 1,
+          synopsis: 1,
+        })
+        .toArray();
 
-        res.json({movies});
-
-    } catch(error){
-        console.log("error fetching movies" ,error);
-        res.status(401).json({ Error: "Error"});
-    };
+      res.json({ movies });
+    } catch (error) {
+      console.log("error fetching movies", error);
+      res.status(401).json({ Error: "Error" });
+    }
   });
 
   //findOne movie
-  app.get("/movies/:id", async (req,res) => {
-    try{
-        const id = req.params.id;
-        console.log(id);
-        const movie = await db.collection("movies").findOne(
-            {_id: new ObjectId(id)},
-            {projection: {_id:0, categories:0}}
-        ); 
-        console.log(movie);
-        if(!movie){
-            res.status(401).json({Error: "Error"});
-        };
+  app.get("/movies/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      console.log(id);
+      const movie = await db
+        .collection("movies")
+        .findOne(
+          { _id: new ObjectId(id) },
+          { projection: { _id: 0, categories: 0 } }
+        );
+      console.log(movie);
+      if (!movie) {
+        res.status(401).json({ Error: "Error" });
+      }
 
-        res.json({movie});
-
-    }catch(error){
-        console.log("error fetching movies" ,error);
-        res.status(401).json({ Error: "Error"});
-    };
+      res.json({ movie });
+    } catch (error) {
+      console.log("error fetching movies", error);
+      res.status(401).json({ Error: "Error" });
+    }
   });
-
-  app.get("/movies", async (req,res) =>{
+  
+  //Search Movies
+  app.get("/movies", async (req, res) => {
 
   });
-
-
 }
 main();
 
